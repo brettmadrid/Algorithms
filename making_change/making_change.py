56ca4cd4e4a82f9c9a5f87dd494a0aef1b74f4e4
brettmadrid <brettmadrid9@gmail.com>
2020-03-05 23:47:10 -0800

    added comments to making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,23 +2,39 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  count = [0 for x in range(amount+1)] 
-  count[0] = 1
-  
-  for i in range(0,len(denominations)):
-    for j in range(denominations[i], amount+1):
-      count[j] += count[j-denominations[i]]
-      
-  return count[amount]
+    # create a zero padded list size of incoming amount + 1
+    count = [0 for x in range(amount+1)]
+
+    # set first array position value to equal 1
+    count[0] = 1
+
+    # loop through all possible coin values in denominations array
+    for i in range(0, len(denominations)):  # for 0 to 4
+
+        for j in range(denominations[i], amount+1):
+
+            count[j] += count[j-denominations[i]]
+
+    return count[amount]
+
+# i = 2       to 4
+# j = 10       to 5
+# count = [1,1,1,1,1,2]
+# count[5] += count[0] = 1
 
 
-if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+denominations = [1, 5, 10, 25, 50]
+amount = 300
+print(making_change(amount, denominations))
+
+# if __name__ == "__main__":
+#   # Test our your implementation from the command line
+#   # with `python making_change.py [amount]` with different amounts
+#   if len(sys.argv) > 1:
+#     denominations = [1, 5, 10, 25, 50]
+#     amount = int(sys.argv[1])
+#     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+#   else:
+#     print("Usage: making_change.py [amount]")
