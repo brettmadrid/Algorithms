77b73702c44dc452a474aaf4235549a33b806616
brettmadrid <brettmadrid9@gmail.com>
2020-03-08 18:21:00 -0700

    added comments

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,32 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    output = []
+    possible_plays = ['scissors', 'paper', 'rock']
+
+    stack = []
+    stack.append([])
+
+    while len(stack) > 0:
+        hand = stack.pop()
+        print(hand)
+        # then record hand in output array
+        if n == 0 or len(hand) == n:
+            output.append(hand)
+        else:
+            for play in possible_plays:
+                stack.append(hand + [play])
+
+    return output
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+rock_paper_scissors(2)
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_plays = int(sys.argv[1])
+#         print(rock_paper_scissors(num_plays))
+#     else:
+#         print('Usage: rps.py [num_plays]')
